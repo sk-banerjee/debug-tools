384f54b42f0a0bb978b1e18caf937e7db7db1ed0
Sourabh Banerjee <banerjee.sourabh@gmail.com>
2022-01-24 00:32:44 +0530

    add valgrind-leak-examples.cpp

---
+++ valgrind/valgrind-leak-examples.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <sys/mman.h>
+#include <cstring>
+
+using namespace std;
+
+#define MMAP_BUF_LEN   (2048)
+#define ALLOC_BUF_LEN  (4096)
+
+void * my_alloc(size_t sz, int prot, int flags) {
+    void * ptr = mmap(NULL, sz, prot, flags, 0, 0);
+    if (ptr == MAP_FAILED) {
+        cout << "mmap() failed" << endl;
+        return NULL;
+    }
+    return ptr;
+}
+
+int my_free(void *ptr, size_t sz) {
+    int err = munmap(ptr, sz);
+    if (err != 0) {
+        cout << "munmap() failed" << endl;
+        return -1;
+    }
+    return 0;
+}
+
+int main(int argc, char **argv) {
+    void *ptr = nullptr;
+    bool keep_looping = true;
+    bool simulate_leaks = true;
+
+    while (keep_looping) {
+        ptr = my_alloc( MMAP_BUF_LEN, PROT_READ | PROT_WRITE,
+                        MAP_PRIVATE | MAP_ANONYMOUS);
+        if (ptr)
+            memset(ptr, '0', MMAP_BUF_LEN);
+        if (!simulate_leaks)
+            my_free(ptr, MMAP_BUF_LEN);
+
+        ptr = my_alloc( MMAP_BUF_LEN, PROT_READ | PROT_WRITE,
+                        MAP_SHARED | MAP_ANONYMOUS);
+        if (ptr)
+            memset(ptr, '1', MMAP_BUF_LEN);
+        if (!simulate_leaks)
+            my_free(ptr, MMAP_BUF_LEN);
+
+        ptr = malloc(ALLOC_BUF_LEN);
+        if (ptr)
+            memset(ptr, '2', ALLOC_BUF_LEN);
+        if (!simulate_leaks)
+            free(ptr);
+
+        char *p = new char[ALLOC_BUF_LEN];
+        if (ptr)
+            memset(ptr, '3', ALLOC_BUF_LEN);
+        if (!simulate_leaks)
+            delete[] p;
+    }
+    return 0;
+}
