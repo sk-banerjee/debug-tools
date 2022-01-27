26c2cee7586a3601678154543d4834270b1be6f8
Sourabh Banerjee <banerjee.sourabh@gmail.com>
2022-01-27 20:16:49 +0530

    leak_type_example.cpp: add more valgrind leak type examples
      * possible lost
      * still reachable

--- valgrind/leak_type_example.cpp
+++ valgrind/leak_type_example.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
+#include <cstring>
+#include <thread>
 
 using namespace std;
 
 class Node {
     int data;
+    char elements[16];
     Node *next;
 
 public:
@@ -55,25 +58,46 @@ void llist::llist_print( ) {
     cout << endl;
 }
 
+void print_hello() {
+    string s = "Hello World";
+    char* p = strdup(s.c_str()); /* 12 bytes in 1 blocks are definitely lost */
+    *(p + 5) = 0;
+    cout << p;
+    p += 6;
+    cout << " " << p << endl;
+}
+
+void print_world() {
+    string s = "Aloha World";
+    char* p = strdup(s.c_str()); /* 12 bytes in 1 blocks are possibly lost */
+    *(p + 5) = 0;
+    cout << p;
+    p += 6;
+    cout << " " << p << endl; 
+    terminate(); // This call leads to possibly lost line in valgrind logs.
+}
+
 int main() {
-    llist *linked_list_1 = new llist;  /* No nodes from this leak are freed */
-                                       /* Valgrind: 88 (8 direct, 80 indirect) bytes in 1 blocks are definitely */
+    /* No nodes from list 1 leak are freed */
+    llist *linked_list_1 = new llist; /* 8 bytes in 1 blocks are still reachable */
     for (int i = 100; i <= 104; i++) {
-        linked_list_1->llist_insert_at_head(i); /* Valgrind: 80 bytes in 5 blocks are indirectly lost */
+        linked_list_1->llist_insert_at_head(i); /* 160 bytes in 5 blocks are still reachable*/
     }
     linked_list_1->llist_print();
 
-    llist *linked_list_2 = new llist;  /* Head node from this leak is freed */
+    /* Head from this leak is freed */
+    llist *linked_list_2 = new llist; 
     for (int i = 110; i <= 119; i++) {
-        linked_list_2->llist_insert_at_head(i); /* Valgrind: 144 bytes in 9 blocks are indirectly lost*/
-                                                /* Valgrind: 160 (16 direct, 144 indirect) bytes in 1 blocks are definitely lost*/
+        linked_list_2->llist_insert_at_head(i); /* 288 bytes in 9 blocks are indirectly lost */
+                                                /* 320 (32 direct, 288 indirect) bytes in 1 blocks are definitely lost */
     }
     linked_list_2->llist_print();
     delete linked_list_2;
 
-    llist *linked_list_3 = new llist;  /* Mid node from this list is lost track*/
+    /* A mid node from this list is lost track*/
+    llist *linked_list_3 = new llist;  
     for (int i = 120; i <= 134; i++) {
-        linked_list_3->llist_insert_at_head(i); /* Valgrind: 16 bytes in 1 blocks are definitely lost */
+        linked_list_3->llist_insert_at_head(i); /* 32 bytes in 1 blocks are definitely lost */
     }
     linked_list_3->llist_print();
     linked_list_3->llist_simulate_leak_at_index(10);
@@ -81,6 +105,13 @@ int main() {
     linked_list_3->llist_free();
     linked_list_3->llist_print();
     delete linked_list_3;
+
+    thread thread_one (print_hello);
+
+    thread thread_two (print_world); /* 16 bytes in 1 blocks are still reachable*/
+                                     /* 288 bytes in 1 blocks are possibly lost */
+    thread_one.join();
+    thread_two.join();
 
     return 0;
 }
